dc0fa53c34dc8581dfca9df9b87b972e1f742830
marce0321 <113398591+marce0321@users.noreply.github.com>
2023-05-31 21:51:46 -0500

    hw react 1

---
+++ src/components/Transactions/Transactions.jsx
@@ -0,0 +1,51 @@
+import css from './Transactions.module.css';
+import PropTypes from 'prop-types';
+
+const createColor = () => {
+  const color =
+    'rgba(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    0.2 +
+    ')';
+
+  return color;
+};
+
+export const TransactionHistory = ({ items }) => {
+  return (
+   
+    <table className={css.transactionHistory}>
+    <thead className={css.head}>
+        <tr style={{ backgroundColor: createColor() }}>
+         <th>Type</th>
+         <th className={css.amount}>Amount</th>
+         <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody className={css.body}>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id} style={{ backgroundColor: createColor() }}>
+              <td className={css.type}>{type}</td>
+              <td className={css.amount}>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
